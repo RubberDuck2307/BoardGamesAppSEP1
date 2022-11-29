492d47f96ee164d55fe08439593637da42104f21
Jan Metela <janmetela@seznam.cz>
2022-11-29 09:54:38 +0100

    AddPlayerPage.fxml

--- src/Controllers/PlayersController.java
+++ src/Controllers/PlayersController.java
@@ -2,6 +2,13 @@ package Controllers;
 
 import Application.ViewHandler;
 import Model.ModelManager;
+import Model.PlayerTable;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.Region;
 
 public class PlayersController implements Controller
@@ -10,6 +17,13 @@ public class PlayersController implements Controller
   ModelManager model;
   ViewHandler viewHandler;
 
+  @FXML private TableView<PlayerTable> playersTable;
+  @FXML public TableColumn<PlayerTable, String> name;
+
+  @FXML public TableColumn<PlayerTable, String> email;
+
+  @FXML public TableColumn<PlayerTable, String> phone;
+  @FXML public TableColumn<PlayerTable, String> membership;
   public PlayersController(){}
   @Override public void init(Region region, ModelManager model,
       ViewHandler viewHandler)
@@ -17,7 +31,19 @@ public class PlayersController implements Controller
     this.region = region;
     this.model = model;
     this.viewHandler = viewHandler;
+    ObservableList<PlayerTable> playersTables = FXCollections.observableArrayList(
+        new PlayerTable("Alex", "5555555555","Alex@Monkey.com","Guest")
+    );
+
+    //make sure the property value factory should be exactly same as the e.g getStudentId from your model class
+    name.setCellValueFactory(new PropertyValueFactory<>("name"));
+    email.setCellValueFactory(new PropertyValueFactory<>("email"));
+    phone.setCellValueFactory(new PropertyValueFactory<>("phone"));
+    membership.setCellValueFactory(new PropertyValueFactory<>("membership"));
+    //add your data to the table here.
+    playersTable.setItems(playersTables);
   }
+
 
   @Override public Region getRegion()
   {
---
+++ src/Model/PlayerTable.java
@@ -0,0 +1,79 @@
+package Model;
+
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+
+public class PlayerTable
+{
+  StringProperty name;
+  StringProperty phone;
+  StringProperty email;
+  StringProperty membership;
+
+  public PlayerTable(String name,String phone, String email, String membership){
+    this.name = new SimpleStringProperty(name);
+    this.phone = new SimpleStringProperty(phone);
+    this.email = new SimpleStringProperty(email);
+    this.membership = new SimpleStringProperty(membership);
+  }
+
+  public String getName()
+  {
+    return name.get();
+  }
+
+  public StringProperty nameProperty()
+  {
+    return name;
+  }
+
+  public void setName(String name)
+  {
+    this.name.set(name);
+  }
+
+  public String getPhone()
+  {
+    return phone.get();
+  }
+
+  public StringProperty phoneProperty()
+  {
+    return phone;
+  }
+
+  public void setPhone(String phone)
+  {
+    this.phone.set(phone);
+  }
+
+  public String getEmail()
+  {
+    return email.get();
+  }
+
+  public StringProperty emailProperty()
+  {
+    return email;
+  }
+
+  public void setEmail(String email)
+  {
+    this.email.set(email);
+  }
+
+  public String getMembership()
+  {
+    return membership.get();
+  }
+
+  public StringProperty membershipProperty()
+  {
+    return membership;
+  }
+
+  public void setMembership(String membership)
+  {
+    this.membership.set(membership);
+  }
+}
