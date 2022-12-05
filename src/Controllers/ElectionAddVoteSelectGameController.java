edd91d8241446058d1eb3e4b891a05edb29ec935
AnnaAndrlova <andrlovaanicka@seznam.cz>
2022-12-05 12:20:24 +0100

    BG filter in elections

--- src/Controllers/ElectionAddVoteSelectGameController.java
+++ src/Controllers/ElectionAddVoteSelectGameController.java
@@ -5,6 +5,7 @@ import Model.BoardGame;
 import Model.BoardGamesList;
 import Model.ConsideredToBeBoughtTable;
 import Model.ModelManager;
+import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
@@ -12,6 +13,7 @@ import javafx.fxml.FXML;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.Region;
 
@@ -26,11 +28,14 @@ public class ElectionAddVoteSelectGameController implements Controller
   @FXML public TableColumn<ConsideredToBeBoughtTable, String> type;
   @FXML public TableColumn<ConsideredToBeBoughtTable, String> name;
   @FXML public TableColumn<ConsideredToBeBoughtTable, String> numberOfPlayers;
+  public TextField searchField;
+  public TextField numberOfPlayersFilter;
 
   private Region region;
   private ModelManager model;
   private ViewHandler viewHandler;
   private int IDOfPlayer;
+  private String genreValue;
   private ObservableList<ConsideredToBeBoughtTable> gamesInTable = FXCollections.observableArrayList();
   @Override public void init(Region region, ModelManager model,
       ViewHandler viewHandler, int ID)
@@ -41,6 +46,15 @@ public class ElectionAddVoteSelectGameController implements Controller
     IDOfPlayer = ID;
 
     fillData();
+    ObservableList<String> items = FXCollections.observableArrayList(
+        BoardGame.getAllowedTypes());
+    genre.setItems(items);
+
+    genre.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) -> {
+      getGenre(observable, oldValue, newValue);
+      fillData();
+
+    });
 
     name.setCellValueFactory(new PropertyValueFactory<>("name"));
     type.setCellValueFactory(new PropertyValueFactory<>("type"));
@@ -60,8 +74,22 @@ public class ElectionAddVoteSelectGameController implements Controller
   }
 
   @FXML public void fillData(){
-    BoardGamesList boardGamesList = model.getBoardGamesList().getConsideredToBeBoughtGames();
+    gamesInTable.clear();
+    BoardGamesList boardGamesList;
+    boardGamesList = model.getBoardGamesList();
+    boardGamesList = boardGamesList.filterBoardGameList(searchField.getText());
+    String numberString = numberOfPlayersFilter.getText();
 
+    if (!numberString.equals(""))
+    {
+      int number = Integer.parseInt(numberOfPlayersFilter.getText());
+      boardGamesList = boardGamesList.getBoardGameListByNumberOfPlayers(number);
+    }
+
+    if (genreValue != null)
+    {
+      boardGamesList = boardGamesList.getBoardGameListByGenre(genreValue);
+    }
     for (int i = boardGamesList.size() -1 ;  i >= 0; i--)
     {
       BoardGame game = boardGamesList.getBoardGame(i);
@@ -69,6 +97,11 @@ public class ElectionAddVoteSelectGameController implements Controller
       gamesInTable.add(new ConsideredToBeBoughtTable(game.getName(), game.getType(), numberOfPlayers, game.getNumberOfVotes(),
           game.getID()));
     }
+  }
+  public void getGenre(ObservableValue observableValue, Object oldValue, Object newValue)
+  {
+    this.genreValue = newValue.toString();
+
   }
   @FXML public void goBack(){
     viewHandler.openView(33,-1);
