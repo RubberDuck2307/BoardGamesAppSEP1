70066aad395ca35dcc29ad17cd989633cd2d45ba
Jan Metela <janmetela@seznam.cz>
2022-12-08 12:40:51 +0100

    fixing Bugs

--- src/Application/Test.java
+++ src/Application/Test.java
@@ -1,7 +1,9 @@
 package Application;
 
+import Model.BoardGame;
 import Model.Event;
 import Model.EventsList;
+import Model.Reservation;
 
 import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.transform.TransformerException;
@@ -9,6 +11,7 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.util.ArrayList;
+import java.util.Comparator;
 
 public class Test
 {
@@ -16,12 +19,17 @@ public class Test
       throws ParserConfigurationException, TransformerException
   {
     LocalDate fromDate = LocalDate.now();
-    LocalDate toDate = LocalDate.of(2022, 12, 1);
+    LocalDate toDate = LocalDate.of(2022, 12, 9);
+    LocalDate fromDate2 = LocalDate.of(2022, 12, 1);
+    Reservation reservation = new Reservation(0,0,fromDate,toDate,"haha");
+    Reservation reservation1 = new Reservation(0,0, fromDate2, toDate, "caa");
+    ArrayList<Reservation> arrayList = new ArrayList<>();
+    arrayList.add(reservation);
+    arrayList.add(reservation1);
+    arrayList.sort(Comparator.comparing(Reservation::getFrom));
+    System.out.println(arrayList);
 
-    Event event = new Event(0, "name ex", "place ex", fromDate, 2, 3, toDate, 4, 5, "",
-        new ArrayList<>(), "", ""); EventsList eventsList = new EventsList();
-    eventsList.addEvent(event);
-    FileReader.saveEventsList(eventsList);
+
 
   }
 }
